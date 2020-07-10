147e9dc701fa6d0a40bdf201a9d247d955f8e65a
ritesh goswami <you@example.com>
2020-07-10 22:07:30 +0530

    add super method in ES6

--- COMPONENT/ES6/ES6.js
+++ COMPONENT/ES6/ES6.js
@@ -2,6 +2,7 @@ const Topic = [
   { name: 'class' },
   { name: 'Parameterized Constructor' },
   { name: 'class Inheritance' },
+  { name: 'Super Method' },
 ];
 (function () {
   const apply = document.querySelector('#Es_topic');
@@ -36,8 +37,12 @@ export default class Ecmascript {
   Inherit() {
     return `${Inheri}`;
   }
-  Inherit_Ex(){
-    return `${Inheri_Ex}`
+  Inherit_Ex() {
+    return `${Inheri_Ex}`;
+  }
+  // superMethod
+  SuperPro(){
+     return `${supreM}`
   }
 }
 
@@ -52,78 +57,98 @@ const header = `<span class="material-icons code_dot">
     </span>`;
 
 const syntax = `
-            class Name_Of_class (){
-                constructor() {
-                    //properties here , that's call to instance member
-                }
-                method(){
-                   //proto type member here 
-                }  
-            }
-            const name = new Name_Of_class();//this id class of new object`;
+  class Name_Of_class (){
+      constructor() {
+          //properties here , that's call to instance member
+      }
+      method(){
+          //proto type member here 
+      }  
+  }
+  const name = new Name_Of_class();//this id class ofobject`;
 
 const classEx = `
-            class Data (){
-                constructor() {
-                    this.a = 100;
-                    // if want make properties of constructor private 
-                    // so write -> var a = 100; 
-                    // so new obj can not access it's properties
-                }
-                method(){
-                    return 'this method';
-                }  
-            }
-            const name = new data();
-            document.write(name.a); // output -> 100
-            document.write(name.method()); // output -> this method
-            `;
+ class Data (){
+     constructor() {
+         this.a = 100;
+         // if want make properties of constructor private 
+         // so write -> var a = 100; 
+         // so new obj can not access it's properties
+     }
+     method(){
+         return 'this method';
+     }  
+ }
+ const name = new data();
+ document.write(name.a); // output -> 100
+ document.write(name.method()); // output -> this method `;
 const ParaC = `
-           class Data (){
-               constructor(a,b) {
-                   this.name = a;
-                   this.last_name = b;
-               }
-               method(){
-                   return this.name+this.last_name;
-               }  
-           }
-           const name = new data('Robert','Downey');
-           document.write(name.method())//output -> Robert Downey;`;
+  class Data (){
+      constructor(a,b) {
+          this.name = a;
+          this.last_name = b;
+      }
+      method(){
+          return this.name+this.last_name;
+      }  
+  }
+  const name = new data('Robert','Downey');
+  document.write(name.method())//output -> Robert Downey;`;
 // =========Inheritance===============
 const Inheri = `
-            class Parent {
-              // parent class & property
-            }
-            class Child extends Parent {
-              //child use parent class & property
-            }`;
-const Inheri_Ex =`
-             class Country {
-               constructor() {}
-               C_population() {
-                 return '135.26 crores';
-               }
-             }//parent class
+  class Parent {
+    // parent class & property
+  }
+  class Child extends Parent {
+    //child use parent class & property
+  }`;
+const Inheri_Ex = `
+  class Country {
+    constructor() {}
+    C_population() {
+      return '135.26 crores';
+    }
+  }//parent class
 
-             class State extends Country {
-               S_population() {
-                 return '11.42 crore';
-               }
-             }//child class
+  class State extends Country {
+    S_population() {
+      return '11.42 crore';
+    }
+  }//child class
 
-             class City extends State {
-               City_pop(){
-                 return '31.2 lakhs';
-               }
-             }//Grand child class
+  class City extends State {
+    City_pop(){
+      return '31.2 lakhs';
+    }
+  }//Grand child class
 
-             const Pune = new City();
-             document.write(Pune.City_pop());
-             // Grand child class Object can access owner class methood 
+  const Pune = new City();
+  document.write(Pune.City_pop());
+  // Grand child class Object can access owner class methood 
 
-             document.write(Pune.S_population());
-             // Grand child class Object can access child class methood 
+  document.write(Pune.S_population());
+  // Grand child class Object can access child class methood 
 
-             document.write(Pune.C_population());
-             // Grand child class Object can access parent class methood`;            
+  document.write(Pune.C_population());
+  // Grand child class Object can access parent class methood`;
+
+// ---------------- super method -------
+const supreM = `
+class products {
+  constructor(item1, itemPrice) {
+    this.product = item1;
+    this.price = itemPrice;
+  }
+  addItem() {
+    return this.product + this.price;
+  }
+}
+
+class customer extends products {
+  constructor(item1, itemPrice) {
+    super(item1, itemPrice);
+    //In super methood pass same parameter that pass in parent class constructor parameter 
+  }
+}
+const user = new customer('milk', 1000);
+document.write(user.addItem());`
