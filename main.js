5a65316eeb9176fcdbcb7b4009ce1590f491400e
ritesh goswami <you@example.com>
2020-07-11 16:06:53 +0530

    fix nav bar bug

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+function openList() {
+  let open = document.querySelector('.list_box');
+  open.style.display= "block";
+}
+function closeList() {
+  let open = document.querySelector('.list_box');
+  open.style.display= "none"; 
+}
+{/* <span class="material-icons" onclick="closeList()">
+clear
+</span></button> */}
