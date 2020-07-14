07acb64afbb502ff5f666b8753858a229e7cd59f
ritesh goswami <you@example.com>
2020-07-14 17:26:21 +0530

    fixed topic bar & add BG in backgound img

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-var x = window.matchMedia('(max-width: 700px)');
-if (x.matches) {
-  // If media query matches
-  function openList() {
-    let list_Box = document.querySelector('.list_box');
-    list_Box.style.display = 'block';
-  }
-  function closeList() {
-    let list_Box = document.querySelector('.list_box');
-    list_Box.style.display = 'none';
-  }
-} else {
-}
 
 // --------------render for topic list -----------------
 export default class GetTopicList {
--- src/assets/Js/sidenav.js
+++ src/assets/Js/sidenav.js
@@ -17,3 +17,17 @@ function Nclose() {
   NavElement.side_bar.classList.add('nav_side');
   NavElement.side_bar.classList.remove('nav_side_remove');
 }
+
+var x = window.matchMedia('(max-width: 700px)');
+if (x.matches) {
+  // If media query matches
+  function openList() {
+    let list_Box = document.querySelector('.list_box');
+    list_Box.style.display = 'block';
+  }
+  function closeList() {
+    let list_Box = document.querySelector('.list_box');
+    list_Box.style.display = 'none';
+  }
+} else {
+}
