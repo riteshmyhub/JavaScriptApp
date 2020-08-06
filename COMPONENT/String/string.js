c098b3b7b51bc1f196cf8abe6969162640d50f0f
ritesh goswami <you@example.com>
2020-08-07 00:06:33 +0530

    end array topic & string>intro

---
+++ COMPONENT/String/string.js
@@ -0,0 +1,5 @@
+import SorceCode from '../../source_code_render';
+export class StringTopic extends SorceCode {
+
+}
+// _________source code_________
---
+++ test/test.js
@@ -0,0 +1,17 @@
+// String in JavaScript
+// group of character is called string ex - "hi how are you"
+// if any number writer in double court so that is not a number It's string,
+//  write string in three type  1 - " "  2 -''  3-``
+
+// string assignment way_____
+
+// in primitve -
+const test = 'hello';
+// primitve is "string" type
+
+// in constructor
+const name = new String("hello");
+document.write(name);
+// constructor in object type
+// execution its String constructor slow
+
