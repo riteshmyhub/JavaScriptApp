0a9197cf89852244eb55d0201313a625a5c0cf00
ritesh goswami <you@example.com>
2020-08-07 23:39:53 +0530

    add  string Concatenation

--- COMPONENT/String/string.js
+++ COMPONENT/String/string.js
@@ -1,5 +1,11 @@
-import SorceCode from '../../source_code_render';
-export class StringTopic extends SorceCode {
-
+import SourceCode from '../../source_code_render.js';
+export default class StringTopic extends SourceCode {
+  Conc() {
+    return Concatenation;
+  }
 }
 // _________source code_________
+const Concatenation = `
+const test = 'if you happy and ';
+const data = test.concat('you ', 'know ', 'say ', 'meow '); //must declare in variable
+document.write(data);`;
