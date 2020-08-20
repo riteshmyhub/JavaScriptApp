b7dd9e17042469d3ab185538214f0bb90af63473
ritesh goswami <you@example.com>
2020-08-21 00:13:32 +0530

    string > method & property

--- COMPONENT/String/Strings.js
+++ COMPONENT/String/Strings.js
@@ -3,8 +3,35 @@ export default class StringTopic extends SourceCode {
   Conc() {
     return Concatenation;
   }
-  BackT(){
-  return interpolation;
+  BackT() {
+    return interpolation;
+  }
+  strL() {
+    return var_strLength;
+  }
+  CharAA() {
+    return var_CharAA;
+  }
+  CharCodes() {
+    return var_CharCode;
+  }
+  StrCase() {
+    return var_StrCase;
+  }
+  Strtrim() {
+    return var_Strtrim;
+  }
+  StrReplace() {
+    return var_StrReplace;
+  }
+  StrSplit() {
+    return var_StrSplit;
+  }
+  StrIndexOf() {
+    return var_StrIndexOf;
+  }
+  StrSearch() {
+    return var_StrSearch;
   }
 }
 // _________source code_________
@@ -14,8 +41,8 @@ const data = test.concat('you ', 'know ', 'say ', 'meow '); //must declare in va
 document.write(data);`;
 
 let name = "'Alex'";
-let CR = "`$&#123;"
-let CL = "&#125;`"
+let CR = '`$&#123;';
+let CL = '&#125;`';
 
 const interpolation = `
 const user =  ${name};
@@ -25,3 +52,43 @@ console.log(call functions ,${CR}method()${CL})// call functions
 console.log(call functions ,${CR}voluptatum assumenda 
 quidem dignissimos
  officiis${CL})// multiple line`;
+
+const var_strLength = `
+let str = 'hello , jS';
+console.log(str.length);`;
+
+const var_CharAA = `
+let str2 = 'hello , JavaScript';
+console.log(str2.charAt(10));`;
+
+const var_CharCode = `
+let str3 = 'hello , JavaScript';
+console.log(str3.charCodeAt(8));`;
+
+const var_StrCase = `
+let str4 = 'hello , JavaScript';
+console.log(str4.toUpperCase());
+console.log(str4.toLowerCase());`;
+
+const var_Strtrim = `
+let str5 = '     hello , JavaScript    ';
+console.log(str5.trim());`;
+
+const var_StrReplace = `
+let str6 = 'hello , JavaScript';
+console.log(str6.replace('JavaScript', 'Es7'));`;
+
+const var_StrSplit = `
+let text = 'my name is alex';
+let arr = text.split(' '); //  here argument is space
+console.log(arr);
+document.write(arr[1]);`;
+
+const var_StrIndexOf = `
+let indexTexts = 'in split method pass argument and';
+console.log(indexTexts.indexOf('p', 10)); //output 16`;
+
+const var_StrSearch = `
+let TextSemple = 'this string from java script';
+let findIt = TextSemple.search('from');
+console.log(findIt);// output is 12`;
--- test/test.js
+++ test/test.js
@@ -1,20 +1,58 @@
-// Template Literal or string
-let incri = 1;
-let year = 2000 + incri;
-let Person = 100;
-let subPerson = 0;
-if (year + incri) {
-  subPerson += incri;
-  console.log(subPerson);
-}
-let remaingPerson = Person - subPerson;
+//---------------- String Length Property
+// String Length Property is return length of string
+// sapce is also count in length of string
+let str = 'hello , jS';
+console.log(str.length);
 
-function budget(Str, ...rest) {
-  if (remaingPerson === 0) {
-    Str[3] = 'notething'; 
-  }
-  return `${Str[0]} ${rest[0]} ${Str[1]} ${rest[1]} ${Str[2]} ${rest[2]} ${Str[3]}`;
-}
-document.write(
-  budget`this new budget of Indian if year ${year} so we give subsidy , only ${subPerson}% person but remaing for person ${remaingPerson}% we not able take service`
-);
+//---------------charAt Method
+// this method is return specifics position (index) of string letter
+let str2 = 'hello , JavaScript';
+console.log(str2.charAt(10));
+
+// ------------------charCodeAt
+// this method is return unicode of string letter
+let str3 = 'hello , JavaScript';
+console.log(str3.charCodeAt(8));
+
+//_______________toUpperCase and toLowerCase
+// toUpperCase : this method is convert to string in UpperCase
+// toLowerCase : this method is convert to string in LowerCase
+let str4 = 'hello , JavaScript';
+console.log(str4.toUpperCase());
+console.log(str4.toLowerCase());
+
+// __________________trim Method
+// trim Method is remove whitespace from string
+// this method is only remove whitespace from stating , ending but not between
+let str5 = '     hello , JavaScript    ';
+console.log(str5.trim());
+
+// _______________replace Method
+// IN replace Method pass two argument (old,new)
+//  replace Method is replace to string value and add new value
+// this method is case sensitive
+// it is replace to first match
+// let str6 = 'hello , JavaScript JavaScript';
+
+let str6 = 'hello , JavaScript';
+console.log(str6.replace('JavaScript', 'Es7'));
+//
+
+// --------------------------------------------
+// _________split Method_________________
+//split Method is break to big string in small string than assign in array
+// in split method pass argument and  according argument break to string  i i i i i
+let text = 'my name ritesh';
+let arr = text.split(' '); //  here argument is space
+console.log(arr);
+document.write(arr[1]); // here access string in array item
+
+// indexOf Method
+
+let indexTexts = 'in split method pass argument and';
+console.log(indexTexts.indexOf('p', 10)); //output 16
+
+// serch method
+let TextSemple = 'this string from java script';
+let findIt = TextSemple.search('from');
+console.log(findIt);// output is 12
