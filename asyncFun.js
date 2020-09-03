46a2e239b5872ca13220cc58fa7f27ef0e1448ef
ritesh goswami <you@example.com>
2020-09-04 03:03:31 +0530

    asnyc for mob

--- asyncFun.js
+++ asyncFun.js
@@ -1,7 +1,22 @@
-function name(targetElement,addClass) {
+// asycFun_class
+function asycFun(targetElement, addClass) {
   let classCode = document.querySelector(targetElement);
   setTimeout(() => {
     classCode.classList.add(addClass);
   }, 500);
 }
-name('#asyncClass','BGI');
+asycFun('#asyncClass', 'BGI');
+// asycFun_code
+function asycFun_code(objs) {
+  let Elemets = document.querySelector(objs.ClassNames);
+  setTimeout(() => {
+    Elemets.innerHTML = objs.template;
+  }, 500);
+}
+asycFun_code({
+ ClassNames:'.Asyncimg',
+ template:`
+ <figure>
+<img src="./src/assets/img/logo.png" alt="" srcset="" class="img-fluid will_only_mobile" width="260">
+</figure>`
+});
